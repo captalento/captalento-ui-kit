1bf4a04ecfc77ec47e7b0ec718fb52ffc0724759
Isaias Mendes <isaiaszmendes@gmail.com>
2020-06-13 16:55:26 -0300

    add ui-kit

---
+++ stories/Input.stories.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { MdSearch } from 'react-icons/md';
+
+import { Input } from '../src';
+
+storiesOf('Components | Input', module)
+  .add('Default', () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+      <Input
+        name="captalento"
+        placeholder="Captalento..."
+        value={inputValue}
+        setValue={setInputValue}
+      />
+    );
+  })
+  .add('With Icon', () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+      <Input
+        name="captalento"
+        icon={<MdSearch />}
+        placeholder="Captalento..."
+        value={inputValue}
+        setValue={setInputValue}
+      />
+    );
+  });
