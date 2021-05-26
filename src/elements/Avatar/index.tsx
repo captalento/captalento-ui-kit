1932cea001f953b2b6113aea4efc3ac329cb6e65
Ygor Mendes <ygortmendes@gmail.com>
2021-05-26 15:52:24 -0300

    Create element img user or first letter of name

---
+++ src/elements/Avatar/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Text } from '../Text';
+
+import { FaUser } from 'react-icons/fa';
+import { Size } from '../../common';
+
+interface User {
+  initiall: string;
+  img: {
+    src: string | undefined;
+    alt: string;
+  };
+}
+
+export interface PropsAvatar extends React.HTMLAttributes<HTMLDivElement> {
+  user: User;
+  sizeText?: Size;
+  sizeIcon?: number;
+}
+
+function Avatar({ user, sizeText, sizeIcon, className, ...rest }: PropsAvatar) {
+  const { img, initiall } = user;
+  const [isLoaded, setIsLoaded] = React.useState<boolean>(false);
+
+  return (
+    <>
+      {isLoaded ? (
+        <div
+          {...rest}
+          className={`${className} w-12 h-12 flex justify-center items-center rounded-full bg-black-100`}
+        >
+          {!img.src && !initiall ? (
+            <FaUser size={sizeIcon} className="text-green-100" />
+          ) : (
+            <Text variant="green" size={sizeText} className={`font-semibold`}>
+              {initiall}
+            </Text>
+          )}
+        </div>
+      ) : (
+        <img
+          src={img.src}
+          alt={img.alt}
+          className={`${className} w-12 rounded-full`}
+          onError={() => setIsLoaded(true)}
+          onLoad={() => setIsLoaded(false)}
+          {...rest}
+        />
+      )}
+    </>
+  );
+}
+
+export { Avatar };
