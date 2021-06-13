aeaa2d866b78f8a1b134b477f037b28690618de8
Ygor Mendes <ygortmendes@gmail.com>
2021-06-13 15:55:56 -0300

    Fix width of the component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,16 +15,18 @@ function Header({ className, application = 'app', children }: headerProps) {
   const classes = classNames('header', className);
 
   return (
-    <header className={classes}>
-      <Link
-        to={application === 'app' ? '/home' : '/'}
-        className="cursor-pointer z-20"
-      >
-        <LogoDesktop className="hidden lg:block h-8" />
-        <LogoMobile className="block lg:hidden h-8" />
-      </Link>
+    <header className="w-full flex justify-center bg-black-300">
+      <div className={classes}>
+        <Link
+          to={application === 'app' ? '/home' : '/'}
+          className="cursor-pointer z-20"
+        >
+          <LogoDesktop className="hidden lg:block h-8" />
+          <LogoMobile className="block lg:hidden h-8" />
+        </Link>
 
-      {children}
+        {children}
+      </div>
     </header>
   );
 }
